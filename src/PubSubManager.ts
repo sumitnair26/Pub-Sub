9069c03fda360a3b9558f361b1a4caf000df51b4
sumitnair26 <sumit.nair26@gmail.com>
2024-04-30 13:28:25 +0530

    PubSub

---
+++ src/PubSubManager.ts
@@ -0,0 +1,22 @@
+export class PubSubManager {
+    private static instance:PubSubManager
+
+    private constructor() {
+
+    }
+
+    public static getInstance(): PubSubManager {
+        if(!PubSubManager.instance){
+            PubSubManager.instance = new PubSubManager()
+        }
+        return PubSubManager.instance
+    }
+
+    addUserToStock(userId:string, stockTicker:string){
+
+    }
+
+    removeUserFromStock(userId:String, stockTicker:string){
+        
+    }
+}
