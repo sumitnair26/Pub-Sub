6a4ca91af9da42f12d566628a804c1e115a79529
sumitnair26 <sumit.nair26@gmail.com>
2024-04-30 16:21:00 +0530

    added pubSubManager class with Subscribe and UnSubscribe functions

--- src/PubSubManager.ts
+++ src/PubSubManager.ts
@@ -1,10 +1,16 @@
+import { createClient, RedisClientType } from "redis"
 export class PubSubManager {
-    private static instance:PubSubManager
+    private static instance:PubSubManager;
+    private redisClient: RedisClientType;
+    private subscriptions: Map<string, string[]>;
 
+    // Private constructor to prevent direct construction calls with the `new` operator
     private constructor() {
-
+        this.redisClient = createClient();
+        this.redisClient.connect();
+        this.subscriptions = new Map();
     }
-
+    // The static method that controls the access to the singleton instance
     public static getInstance(): PubSubManager {
         if(!PubSubManager.instance){
             PubSubManager.instance = new PubSubManager()
@@ -12,11 +18,37 @@ export class PubSubManager {
         return PubSubManager.instance
     }
 
-    addUserToStock(userId:string, stockTicker:string){
-
+    userSubscribe(userId:string, stock:string){
+        if (!this.subscriptions.has(stock)) {
+            this.subscriptions.set(stock, []);
+        }
+        this.subscriptions.get(stock)?.push(userId);
+        if (this.subscriptions.get(stock)?.length===1) {
+            this.redisClient.subscribe(stock, (message)=>{
+                this.handleMessage(stock, message)
+            })
+        }
     }
 
-    removeUserFromStock(userId:String, stockTicker:string){
-        
+    // Define the method that will be called when a message is published to the subscribed channel
+    private handleMessage(stock: string, message: string){
+        console.log(`Message received on channel ${stock}: ${message}`);
+        this.subscriptions.get(stock)?.forEach((sub)=>{
+            console.log(`Sending message to user: ${sub}`);
+        })
+    }
+
+
+    userUnSubscribe(userId:String, stock:string){
+        this.subscriptions.set(stock, this.subscriptions.get(stock)?.filter((sub)=> sub !==userId) || []);
+        if(this.subscriptions.get(stock)?.length===0){
+            this.redisClient.unsubscribe(stock);
+            console.log(`UnSubscribed to Redis channel: ${stock}`);
+        }
+    }
+
+    // Cleanup on instance destruction
+    public async disconnect() {
+        await this.redisClient.quit();
     }
 }
